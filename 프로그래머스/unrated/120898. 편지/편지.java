86e3264e92b4b78cd51479a72aba998907d8add7
jay_010304 <glp0509@naver.com>
2023-12-30 10:43:50 +0900

    [level unrated] Title: 편지, Time: 0.02 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120898. 편지/편지.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(String message) {
+        int answer = 0;
+        
+        answer = message.length();
+        
+        
+        return answer * 2;
+    }
+}
