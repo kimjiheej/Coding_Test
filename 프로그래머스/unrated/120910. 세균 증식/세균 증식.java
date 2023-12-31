c37342651839f5b7fee2ccb08c85590c8bc5b497
jay_010304 <glp0509@naver.com>
2023-12-31 11:21:28 +0900

    [level unrated] Title: 세균 증식, Time: 0.03 ms, Memory: 79.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120910. 세균 증식/세균 증식.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int n, int t) {
+        int answer = 0;
+        
+        int i;
+        int j = n;
+        
+        for(i=0; i<t; i++)
+        {
+            j*=2;
+            
+        }
+        return j;
+    }
+}
