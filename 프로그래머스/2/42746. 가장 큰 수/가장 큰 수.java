cdd48a28ed23070cd3d6b56206ce1e27a078acd9
jay_010304 <glp0509@naver.com>
2024-01-09 09:32:07 +0900

    [level 2] Title: 가장 큰 수, Time: 2.18 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Solution {
+    public String solution(int[] numbers) {
+            String answer = "";
+        boolean except = true;
+        String[] num = new String[numbers.length];
+        
+        for(int i = 0; i < num.length; i++){
+            num[i] = String.valueOf(numbers[i]);        
+            if(numbers[i] != 0) 
+                except = false;
+        }
+        
+        Arrays.sort(num, new Comparator<String>(){
+           @Override
+            public int compare(String n1, String n2) {
+                return (n2+n1).compareTo(n1+n2);
+            }
+        });
+        for(int i = 0; i < num.length; i++)
+            answer += num[i];
+        if(except) answer = "0";
+        return answer;
+    }
+}
