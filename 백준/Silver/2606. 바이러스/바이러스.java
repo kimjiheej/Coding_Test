58d53447710a74517ccd767183439a5bfd3e4b12
jay_010304 <glp0509@naver.com>
2024-01-30 15:52:30 +0900

    [Silver III] Title: 바이러스, Time: 124 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,60 +1,51 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-
-
-// DFS 로 풀 수 있는 문제입니다.
 public class Main {
 
-    static boolean[] visited;
-    static ArrayList<Integer>[] A;
+    static int map[][];
+    static boolean visitied[];
+
     static int count = 0;
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        int com = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
 
-        int computer = Integer.parseInt(st.nextToken());
+        map = new int[com+1][com+1];
+        visitied = new boolean[com+1];
 
-        st = new StringTokenizer(br.readLine());
-        int num = Integer.parseInt(st.nextToken());
+        for(int i=0; i<n; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
 
-        visited = new boolean[computer +1 ];
-        A = new ArrayList[computer+1];
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
 
-        for(int i=1; i<computer+1; i++)
-            A[i] = new ArrayList<Integer>();
+            map[a][b] = 1;
+            map[b][a] = 1;
 
-        for(int i=0; i<num; i++)
-        {
-            st = new StringTokenizer(br.readLine());
-            int s = Integer.parseInt(st.nextToken());
-            int e = Integer.parseInt(st.nextToken());
-
-            A[s].add(e);
-            A[e].add(s);
         }
 
-        DFS(1);
+        dfs(1);
+
 
         System.out.println(count-1);
-
     }
 
-    public static void DFS(int v){
-
-        if(visited[v] == true)
-            return;
-
-        visited[v] = true;
+    public static void dfs(int u){
+        visitied[u] = true;
         count++;
-        
-            for(int i : A[v])
-            {
-                if(visited[i]== false)
-                DFS(i);
+
+        for(int i=1; i<visitied.length; i++){
+            if(map[u][i] == 1 && !visitied[i])
+                dfs(i);
         }
+
     }
+
 }
