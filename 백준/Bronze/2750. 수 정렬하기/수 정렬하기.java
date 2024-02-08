c0a62b1fd4dd4fd67ac3e83c860e42f6b559e7cf
jay_010304 <glp0509@naver.com>
2024-02-08 12:48:50 +0900

    [Bronze II] Title: 수 정렬하기, Time: 176 ms, Memory: 14892 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,38 +1,31 @@
-
-import java.util.Scanner;
+import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
 
-    // algo1874
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int n = sc.nextInt();
-
-        int i, j;
-
-        int array[] = new int[n];
-
-        for (i = 0; i < n; i++) {
-            array[i] = sc.nextInt();
-        }
-        int a, b;
+    public static void main(String[] args) throws IOException {
 
 
-        for (i = 0; i < array.length - 1; i++) {
-            for (j = 0; j < array.length - 1 - i; j++) {
-                if (array[j] > array[j + 1]) {
-                    a = array[j];
-                    b = array[j + 1];
-                    array[j] = b;
-                    array[j + 1] = a;
-                }
-            }
+        // 오름차순으로 정렬하여라
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int c = Integer.parseInt(br.readLine());
+
+        int[] array = new int[c];
+
+        for(int i=0; i<c; i++){
+            array[i] = Integer.parseInt(br.readLine());
         }
 
-        for(i=0; i<array.length;i++){
-            System.out.println(array[i]);
+        Arrays.sort(array);
+
+        for(int a : array){
+            System.out.println(a);
         }
+
     }
 }
