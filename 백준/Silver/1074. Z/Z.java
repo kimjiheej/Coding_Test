3254337996f3d154548df86034137a26e3f56e38
jay_010304 <glp0509@naver.com>
2024-02-09 16:27:32 +0900

    [Silver I] Title: Z, Time: 124 ms, Memory: 14344 KB -BaekjoonHub

--- 백준/Silver/1074. Z/Z.java
+++ 백준/Silver/1074. Z/Z.java
@@ -5,43 +5,41 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static int count = 0;
-
-
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken()); // n 이다
-        int r = Integer.parseInt(st.nextToken()); // r 행
-        int c = Integer.parseInt(st.nextToken()); // c 열 의미
-        int size = (int) Math.pow(2,N);
+        int a = Integer.parseInt(st.nextToken()); // 제곱의 수
+        int b = Integer.parseInt(st.nextToken()); // r 행
+        int c = Integer.parseInt(st.nextToken()); // c 행
 
-        find(size,r,c);
-        System.out.println(count);
+        int miro = check(a, b, c);
+
+        System.out.println(miro);
     }
 
-    public static void find(int size, int r, int c){
-          if(size==1)
-              return;
+    public static int check(int n, int r, int c) {
 
-          if(r < size / 2 && c < size /2)
-              find(size/2,r,c);
-
-        else if(r < size / 2 && c >=size / 2) {
-            count += size * size / 4;
-              find(size / 2, r, c - size / 2);
-          }
-        else if(r >=size/2 && c < size/2){
-            count += (size * size / 4) * 2;
-            find(size / 2, r - size/2, c);
-
+        if (n == 0) {
+            return 0;
         }
-        else {
-            count += (size * size / 4) * 3;
-            find(size/2, r- size/2, c-size/2);
-          }
+
+        int half = 1 << (n - 1); // 제곱 계산을 비트 연산으로 변경
+
+        // 1 사분면에 있을 때
+        if (r < half && c < half)
+            return check(n - 1, r, c);
+
+        if (r < half && c >= half) // 2사분면
+            return half * half + check(n - 1, r, c-half);
+
+        if (r >= half && c < half) // 3번
+            return 2 * half * half + check(n - 1, r-half, c);
+
+        // (b >= half && c >= half)인 경우
+        return 3 * half * half + check(n - 1, r - half, c - half);
     }
 }
+
