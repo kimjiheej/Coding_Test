8ebb99168936607b9a8e606dc80925558153f88b
jay_010304 <glp0509@naver.com>
2024-02-18 13:36:05 +0900

    [Gold V] Title: 탑, Time: 740 ms, Memory: 82108 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -3,36 +3,43 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
 import java.util.StringTokenizer;
-class Info {
-	int height;
-	int index;
-	Info(int height, int index) {
-		this.height = height;
-		this.index = index;
-	}
-}
+
 public class Main {
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(bf.readLine());
-		int[] arr = new int[N];
-		String s = bf.readLine();
-		StringTokenizer st = new StringTokenizer(s, " ");
-		Stack<Info> top = new Stack<>();
-		for(int i = 0; i < arr.length; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-			while(!top.isEmpty() && top.peek().height < arr[i]) {
-				top.pop();
-			}
-			if(top.isEmpty())
-			{
-				System.out.print("0 ");
-			}
-			else
-			{
-				System.out.print(top.peek().index + " ");
-			}
-			top.push(new Info(arr[i], i + 1));
-		}
-	}
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n  = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] array = new int[n];
+        int[] answer = new int[n];
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i = 0; i < n; i++){
+            array[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i = 0; i < n; i++){
+            while (!stack.isEmpty()) {
+                if (array[stack.peek()] < array[i]) {
+                    stack.pop();
+                } else {
+                    answer[i] = stack.peek() + 1;
+                    break;
+                }
+            }
+
+            stack.push(i);
+        }
+
+        for(int i = 0; i < n; i++){
+            sb.append(answer[i]).append(" ");
+        }
+
+        System.out.println(sb.toString());
+    }
 }
