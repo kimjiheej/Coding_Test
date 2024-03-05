8312d15f9ab47a64ba124be71ee2fb3ebcea2c2f
jay_010304 <glp0509@naver.com>
2024-03-05 18:00:15 +0900

    [level 2] Title: 기능개발, Time: 0.32 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,23 +2,37 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
+        int[] answer;
+
         Queue<Integer> queue = new LinkedList<>();
-        for(int i=0; i<progresses.length; i++){
-            queue.add((int) (Math.ceil((100.0 - progresses[i]) / speeds[i])));
+        Queue<Integer> ans = new LinkedList<>();
+
+        for (int i = 0; i < progresses.length; i++) {
+            queue.add((int) Math.ceil((100 - progresses[i]) / (double) speeds[i]));
         }
-        
-        List<Integer> answer = new ArrayList<>();
-        while (!queue.isEmpty()){
-            int day = queue.poll();
-            int cnt = 1;
-            
-            while(!queue.isEmpty() && day >= queue.peek()){
-                cnt++;
-                queue.poll();
+
+        int count = 1;
+        int k = queue.poll(); // 첫 번째 작업의 개발 기간
+        while (!queue.isEmpty()) {
+            int next = queue.poll();
+
+            if (k >= next) {
+                count++; // 현재 작업의 개발이 완료된 후에 다음 작업의 개발 기간을 계산
+            } else {
+                ans.add(count);
+                count = 1; // 새로운 작업의 개발이 완료된 경우 count 초기화
+                k = next;
             }
-            answer.add(cnt);
         }
-        
-        return answer.stream().mapToInt(Integer::intValue).toArray();
+        ans.add(count); // 마지막에 남은 작업 추가
+
+        answer = new int[ans.size()];
+
+        int cc = 0;
+        while (!ans.isEmpty()) {
+            answer[cc++] = ans.poll();
+        }
+
+        return answer;
     }
 }
