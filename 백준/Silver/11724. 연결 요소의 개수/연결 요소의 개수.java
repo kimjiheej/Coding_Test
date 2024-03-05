864cf5c5102ba15d58d923a5a5d1c5852d4681ce
jay_010304 <glp0509@naver.com>
2024-03-05 20:21:41 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 520 ms, Memory: 115884 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,56 +1,78 @@
 import java.io.*;
-import java.util.*;
+import java.util.StringTokenizer;
+
 public class Main {
 
+     static int[][] graph;
+
+     static boolean visitied[];
+
+     static int N,M;
+
+     static int answer;
+
+     static int count = 0;
 
 
-    public static int[][] map ; // 노드가 연결된 정보
-    public static boolean[] visited; // 노드 방문 여부
-    public static int result = 0; // 연결된 노드 횟수
 
-    public static void solution(){
-    
-        for(int i=1; i<visited.length;i++){
-            if(!visited[i])
-            {
-                dfs(i);
-                result++;
-            }
-        }
-    }
-    public static void dfs(int u){
-        visited[u] = true;
-        
-        for(int i=1; i<visited.length;i++){
-            if(map[u][i] == 1 && !visited[i])
-                dfs(i);
-        }
-    }
 
-    public static void main (String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        map = new int[n+1][n+1];
-        visited = new boolean[n+1];
 
-        for (int i = 0; i < m; i++) {
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        // 그래프에 모든 정보를 채운다.
+
+        visitied = new boolean[N+1];
+        graph = new int[N+1][N+1];
+
+        for(int i=0; i<M; i++){
             st = new StringTokenizer(br.readLine());
-            int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
 
-            map[u][v] = 1;
-            map[v][u] = 1;
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            graph[a][b] = 1;
+            graph[b][a] = 1;
+
+
         }
 
-        solution();
-        bw.write(result+"");
 
-        br.close();
-        bw.close();
-    }
+        for(int i = 1; i<=N; i++){
+            if(visitied[i] == false) {
+                dfs(i);
+                count++;
+            }
+        }
+
+        System.out.println(count);
+
+
+
     }
 
+     public static void dfs (int a){
+
+        visitied[a] = true;
+
+
+        for(int i=1; i<=N; i++){
+            if(!visitied[i] && graph[a][i] == 1)
+                dfs(i);
+        }
+
+
+
+     }
+}
+
+/*
+ 핵심 키워드는 연결 요소이다. 방향 없는 그래프. 이것은 그래프를 다루어야 하는구나 !
+ 그리고 연결된 요소를 구해야 하는구나 !
+ */
