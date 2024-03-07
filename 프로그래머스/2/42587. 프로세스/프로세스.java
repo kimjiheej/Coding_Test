efbe7d1bd4516f31d1e0584dd1b0aeefb53d70c2
jay_010304 <glp0509@naver.com>
2024-03-07 13:16:56 +0900

    [level 2] Title: 프로세스, Time: 0.78 ms, Memory: 70.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] priorities, int location) {
+        int answer = 0;
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+        
+        for(int i : priorities){
+            queue.offer(i);
+        }
+        
+        while(!queue.isEmpty()){
+            for(int i=0; i<priorities.length; i++){
+                if(queue.peek() == priorities[i]){
+                    queue.poll();
+                    answer++;
+                    
+                    if(location == i)
+                        return answer;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
