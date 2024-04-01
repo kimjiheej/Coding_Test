12be2de2d97c53ca512cbaecb4542baa73398e8c
jay_010304 <glp0509@naver.com>
2024-04-01 10:56:56 +0900

    [Bronze II] Title: 애너그램 만들기, Time: 204 ms, Memory: 17564 KB -BaekjoonHub

--- 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.java
+++ 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.java
@@ -1,35 +1,37 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
+
 
     public static void main(String[] args) throws IOException {
 
         Scanner sc = new Scanner(System.in);
+
         String a = sc.next();
         String b = sc.next();
 
-        int[] a_count = get_alphabet_count(a);
-        int[] b_count = get_alphabet_count(b);
+        int answer = 0;
 
-        int ans = 0;
+        int[] countA = new int[26]; // dared [1,0,0,2 ..]
+        int[] countB = new int[26];
 
-        for(int i=0; i<26; i++){
-            ans +=Math.abs(a_count[i] - b_count[i]);
+        for(int i=0; i<a.length(); i++){
+            countA[a.charAt(i)-'a']++;
         }
 
-        System.out.println(ans);
-    }
-
-    public static int[] get_alphabet_count(String str){
-        int[] count = new int[26];
-
-        for(int i=0; i<str.length();i++){
-            count[str.charAt(i)-'a']++;
+        for(int i=0; i<b.length(); i++){
+            countB[b.charAt(i)-'a']++;
         }
-        return count;
+
+        for(int i=0; i<countA.length; i++){
+            if(countA[i] != countB[i]){
+                answer += Math.abs(countA[i] - countB[i]);
+            }
+        }
+
+        System.out.println(answer);
     }
 }
