425bcf68c77e5448cb89d9495283a4bb51f761d1
jay_010304 <glp0509@naver.com>
2024-04-04 00:02:57 +0900

    [Silver III] Title: 두 수의 합, Time: 880 ms, Memory: 111388 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -1,42 +1,36 @@
-import java.io.*;
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Main {
 
+
     public static void main(String[] args) throws IOException {
 
+       Scanner sc = new Scanner(System.in);
+       int N = sc.nextInt();
+       int[] a = new int[N];
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       for(int i=0; i<N; i++)
+           a[i] = sc.nextInt();
+       int x = sc.nextInt();
 
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+       boolean[] exist = new boolean[10000001];
+       for(int i=0; i<N; i++)
+           exist[a[i]] = true;
 
-        int n = Integer.parseInt(br.readLine());
+       int ans = 0;
+       for(int i=0; i<N; i++){
+           int pairValue = x-a[i];
 
-        int[] array = new int[n];
+           if(0<=pairValue && pairValue <=1000000)
+               ans += exist[pairValue]? 1 : 0;
+       }
 
+       System.out.println(ans/2);
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i =0; i<n ;i++)
-            array[i] = Integer.parseInt(st.nextToken());
-
-        Arrays.sort(array);
-
-        int x = Integer.parseInt(br.readLine());
-
-        int start = 0;
-        int end = n-1;
-        long count = 0;
-
-        while(start < end){
-            if(array[start]+array[end]==x)
-                count++;
-            if(array[start]+array[end] <=x)
-                start++;
-            else
-                end--;
-        }
-
-        System.out.println(count);
     }
+
 }
