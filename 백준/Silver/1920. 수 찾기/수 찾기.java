37b174bae9e0c52735608ff3a77aadb54b8e29ee
jay_010304 <glp0509@naver.com>
2024-04-16 22:58:42 +0900

    [Silver IV] Title: 수 찾기, Time: 572 ms, Memory: 40892 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,78 +1,58 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    // 이분 탐색 관련 기본 문제
-
-
-    static int arr[];
-    static int array[];
-
-    static int a;
-    static int b;
-
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        a = Integer.parseInt(br.readLine());
 
-        arr = new int[a];
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        for (int i = 0; i < a; i++) {
+        for(int i=0; i<n; i++){
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
         Arrays.sort(arr);
 
-        b = Integer.parseInt(br.readLine());
+        int k = Integer.parseInt(br.readLine());
 
-        array = new int[b];
+        int[] arr1 = new int[k];
 
         st = new StringTokenizer(br.readLine());
 
-        for (int i = 0; i < b; i++) {
-            array[i] = Integer.parseInt(st.nextToken());
+        for(int j=0; j<k; j++){
+            arr1[j] = Integer.parseInt(st.nextToken());
         }
 
-        StringBuilder sb = new StringBuilder();
+        for(int j=0; j<k; j++){
 
-        for (int i = 0; i < b; i++) {
-            if (binarySarch(array[i]) >= 0){
-                sb.append(1).append('\n');
-        }
-            else {
-                sb.append(0).append('\n');
+            int left = 0;
+            int right = arr.length-1;
+            int check = arr1[j];
+
+            while(left <= right){
+                int middle = (left+right)/2;
+
+                if(arr[middle] < check)
+                    left = middle + 1;
+                else if(arr[middle] > check)
+                    right = middle -1;
+                else if(arr[middle] == check){
+                    bw.write(1+"\n");
+                    break;
+                }
             }
-    }
+            if(left > right)
+                bw.write(0+"\n");
+        }
 
-        System.out.println(sb.toString());
-    }
-
-    static int binarySarch(int key){
-
-       int lo = 0;
-       int hi = arr.length -1;
-
-       while(lo <= hi){
-           int mid = (lo + hi )/2;
-
-           if(key < arr[mid]){
-               hi = mid-1;
-           }
-           else if(key > arr[mid]){
-               lo = mid+1;
-           }
-           else {
-               return mid;
-           }
-       }
-       return -1;
+        bw.flush();
+        bw.close();
     }
 }
